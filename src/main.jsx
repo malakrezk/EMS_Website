c72f038ee9f0c85c76fd38c75e91f4b766401eb2
malakrezk <M.Walid2346@nu.edu.eg>
2026-09-25 01:51:56 +0300

    Bundle hero fonts locally

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext'
+import '@fontsource/cormorant-garamond/600.css'
+import '@fontsource/space-grotesk/500.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
